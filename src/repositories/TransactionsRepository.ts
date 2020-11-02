6e43fdf2a4c20e23c181c98ae19e930593c6b976
Bruno Garcia Moura <bgarciamoura@gmail.com>
2020-11-02 16:50:42 -0300

    Optimized the getBalance code

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -24,17 +24,33 @@ class TransactionsRepository {
 	}
 
 	public getBalance(): Balance {
-		const income = this.transactions.reduce((acc, transaction) => {
-			return transaction.type === 'income'
-				? acc + transaction.value
-				: acc + 0;
-		}, 0);
+		// const income = this.transactions.reduce((acc, transaction) => {
+		// 	return transaction.type === 'income'
+		// 		? acc + transaction.value
+		// 		: acc + 0;
+		// }, 0);
 
-		const outcome = this.transactions.reduce((acc, transaction) => {
-			return transaction.type === 'outcome'
-				? acc + transaction.value
-				: acc + 0;
-		}, 0);
+		// const outcome = this.transactions.reduce((acc, transaction) => {
+		// 	return transaction.type === 'outcome'
+		// 		? acc + transaction.value
+		// 		: acc + 0;
+		// }, 0);
+
+		const { income, outcome } = this.transactions.reduce(
+			(accumulator: Balance, transaction: Transaction) => {
+				if (transaction.type === 'outcome') {
+					accumulator.outcome += transaction.value;
+				} else {
+					accumulator.income += transaction.value;
+				}
+				return accumulator;
+			},
+			{
+				income: 0,
+				outcome: 0,
+				total: 0,
+			},
+		);
 
 		const balance = {
 			income,
